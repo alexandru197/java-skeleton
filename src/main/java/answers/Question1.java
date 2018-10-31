da0d1d3883af2048b74d0639e0ce0c63310d0b08
alexandru197 <alexandru197@gmail.com>
2018-10-31 18:56:26 +0000

    Problem 10-first attempt

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,17 @@
 package answers;
+import static java.lang.Math.*;
 
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		// TODO Auto-generated method stub
-		return -1;
+		int max = Integer.MIN_VALUE;
+		for(int i=0;i<portfolios.length;i++){
+			for(int j=1;j<=portfolios.length;j++){
+				max  = Math.max (max, portfolios[i] ^ portfolios[j]);
+			}
+		}
+		return max;
 	}
 
 }
