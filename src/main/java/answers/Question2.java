38a562b4a020ef14ba356141a4200f830a45a2dc
alexandru197 <alexandru197@gmail.com>
2018-10-31 19:50:42 +0000

    Problem 4 first attempt

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -1,10 +1,17 @@
 package answers;
 
+import java.util.Arrays;
+
 public class Question2 {
 
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
-		// TODO Auto-generated method stub
-		return -1;
+		Arrays.sort (cashflowIn);
+		int min = 0;
+		for(int i=cashflowIn.length-1; i>=0;i--){
+
+		}
+
+		return min;
 	}
 
-}
+	}
--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -1,10 +1,34 @@
 package answers;
 
+import static java.lang.Integer.parseInt;
+
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
+		int globalMin = Integer.MAX_VALUE;
+ 		for (int i=0; i<rows.length;i++){
+			int localMin = 0, machines = 0;
+			for(int j=0;j<=rows[i].length;j++){
+				if (machines == numberMachines) globalMin = Math.min(globalMin, localMin);
+
+				if (rows[i][j].equals ('X')) {
+					localMin = 0;
+					machines = 0;
+				}
+				else {
+					if(machines == numberMachines) {
+						globalMin = Math.min(globalMin, localMin);
+						localMin = localMin - parseInt (rows[i][j-numberMachines]) + parseInt (rows[i][j]);
+					}
+					else {
+						localMin += parseInt(rows[i][j]);
+						machines ++;
+					}
+				}
+			}
+			if (machines == numberMachines) globalMin = Math.min(globalMin, localMin);
+		}
+		return globalMin;
 	}
 
 }
